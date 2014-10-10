90bf9ef658457771ddee271c4548c5ce4844ebf3
Luca Trinca <lucatrinca.uk@gmail.com>
2014-10-10 12:21:47 +0100

    Implement left menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,42 @@
-angular.module('Boom', ['ui.router'])
+angular.module('Boom', ['ui.router', 'ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
     'use strict';
-
-    // For any unmatched url, redirect to /
-    $urlRouterProvider.otherwise('/');
-
     $stateProvider
-        .state('base', {
-            url: '',
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: '/templates/menu.html'
+    })
+
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: '/templates/home.html',
+                controller: 'homeController'
+            }
+        }
+    })
+        .state('app.settings', {
+            url: '/settings',
             views: {
-                'header@': {
-                    templateUrl: 'templates/header.html',
+                'menuContent': {
+                    templateUrl: '/templates/settings.html'
                 }
             }
         })
-        .state('base.home', {
-            url: '/',
-            views: {
-                'main@': {
-                    templateUrl: 'templates/home.html',
-                    controller: 'homeController'
-                }
+
+    .state('app.dish', {
+        url: '/dishes/:dishId',
+        views: {
+            'menuContent': {
+                templateUrl: '/templates/dish.html',
+                controller: 'dishController'
             }
-        })
-        .state('base.dish', {
-            url: '/dish/:id',
-            views: {
-                'main@': {
-                    templateUrl: 'templates/dish.html',
-                    controller: 'dishController'
-                }
-            }
-        });
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
 });
