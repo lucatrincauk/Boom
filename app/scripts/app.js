1f6834e1364887e95c60d43f0d6a9912e142b304
Luca Trinca <lucatrinca.uk@gmail.com>
2014-11-23 21:31:40 +0000

    Added routing
    - cleaner index.html, calling only ion-nav-view. ion-nav-view is the
    same as ui-view, but with more ionic functionality (e.g. history)
    - added abstract route that allows a parent container
    - added sample settings route
    - linked navigation with ui-sref
    - added sample footer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,27 +4,54 @@ angular.module('Boom', ['ionic', 'ui.router', 'restangular', 'ionic.rating'])
     'use strict';
 
     // For any unmatched url, redirect to /
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/home');
 
     $stateProvider
-        .state('container', {
-            url: '/',
-            views: {
-                '': {
-                    templateUrl: 'templates/home.html',
-                    controller: 'homeController'
-                },
-                'slideMenu@': {
-                    templateUrl: 'templates/slide-menu.html',
-                    controller: 'slideMenuController'
-                },
-                'header@': {
-                    templateUrl: 'templates/header.html',
-                    controller: 'headerController'
-                },
-                'subheader@': {
-                    templateUrl: 'templates/subheader.html'
-                }
+    //     .state('container', {
+    //         url: '/',
+    //         views: {
+    //             '': {
+    //                 templateUrl: 'templates/home.html',
+    //                 controller: 'homeController'
+    //             },
+    //             'slideMenu@': {
+    //                 templateUrl: 'templates/slide-menu.html',
+    //                 controller: 'slideMenuController'
+    //             },
+    //             'header@': {
+    //                 templateUrl: 'templates/header.html',
+    //                 controller: 'headerController'
+    //             },
+    //             'subheader@': {
+    //                 templateUrl: 'templates/subheader.html'
+    //             }
+    //         }
+    //     });
+
+
+        .state('app', {
+        url: '',
+        abstract: true,
+        templateUrl: 'templates/slide-menu.html'
+    })
+
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/home.html',
+                controller: 'homeController'
             }
-        });
+        }
+    })
+
+    .state('app.settings', {
+        url: '/settings',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/settings.html'
+            }
+        }
+    });
+
 });
