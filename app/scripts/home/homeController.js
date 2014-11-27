015a9685f7b9ef179d00eb4f70ed5816d1b299ba
Luca Trinca <lucatrinca.uk@gmail.com>
2014-11-27 21:47:57 +0000

    Days logic

--- app/scripts/home/homeController.js
+++ app/scripts/home/homeController.js
@@ -6,38 +6,47 @@ angular.module('Boom')
             var day, today, activeDay;
 
             Categories.success(function(data) {
+                $scope.init(data)
+            });
+
+            $scope.init = function(data) {
                 today = new Date();
                 day = today.getDay() - 1;
                 $scope.data = data;
                 $scope.categories = $scope.data[day];
                 activeDay = day;
-                $scope.nameDays();
-            });
 
+                $scope.nameDays();
+
+            }
 
             $scope.nameDays = function() {
-                console.log($scope.categories.day);
-                if ($scope.categories.day == "Thursday") {
-                    $scope.categories.day = "Today";
-                } else if ($scope.categories.day == "Friday") {
-                    $scope.categories.day = "Tomorrow";
-                } else if ($scope.categories.day == "Wednesday") {
-                    $scope.categories.day = "Yesterday";
+                var activeDayName = $scope.categories.day;
+                switch (activeDay) {
+                    case 3:
+                        activeDayName = "Today";
+                        break;
+                    case 4:
+                        activeDayName = "Tomorrow";
+                        break;
+                    case 2:
+                        activeDayName = "Yesterday";
+                        break;
                 }
+                $scope.categories.day = activeDayName;
+
             }
+
             $scope.prev = function(count) {
                 if (activeDay < 1) {
                     return false;
                 } else {
                     activeDay = activeDay - 1;
-
                     $scope.categories = $scope.data[activeDay];
                     $scope.nameDays();
                 }
                 return activeDay;
             };
-
-
 
             $scope.next = function(count) {
 
