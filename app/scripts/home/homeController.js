bbd574e5eefb8bf7867fa044e7e4c1f14e433438
Luca Trinca <lucatrinca.uk@gmail.com>
2014-11-29 16:13:16 +0000

    final changes for flat data setup
    now fully working

--- app/scripts/home/homeController.js
+++ app/scripts/home/homeController.js
@@ -3,29 +3,35 @@ angular.module('Boom')
         function($scope, Categories, Menu) {
             'use strict';
 
+            // Load dishes
             Menu.$loaded().then(function() {
                 $scope.dishes = Menu;
+                $scope.init();
             });
+            // Load categories
             Categories.$loaded().then(function() {
                 $scope.categories = Categories;
             });
 
 
-            var day, today, activeDay;
+            var day, today;
 
+            $scope.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-            $scope.init = function(data) {
+            $scope.init = function() {
+                // get today's date and remove sunday
                 today = new Date();
                 day = today.getDay() - 1;
-                $scope.data = data;
-                if (day > 4) {
-                    $scope.closed = true;
-                    $scope.categories = $scope.data[0];
-                    activeDay = 0;
 
+                // if it's weekend
+                if (day > 4) {
+                    // show closed message
+                    $scope.closed = true;
+                    // set view to Monday
+                    $scope.activeDay = 0;
                 } else {
-                    $scope.categories = $scope.data[day];
-                    activeDay = day;
+                    // otherwise set view to current day
+                    $scope.activeDay = day;
                 }
 
                 $scope.nameDays();
@@ -33,52 +39,51 @@ angular.module('Boom')
             };
 
             $scope.nameDays = function() {
-                switch (day - activeDay) {
+
+                switch (day - $scope.activeDay) {
+                    // If view is same as current day, set it as Today
                     case 0:
-                        $scope.categories.day = 'Today';
+                        $scope.dayName = 'Today';
                         break;
+                        // If view is 1 day behind current day, set it as Yesterday
                     case -1:
-                        $scope.categories.day = 'Tomorrow';
+                        $scope.dayName = 'Tomorrow';
                         break;
+                        // If view is 1 day ahead current day, set it as Tomorrow
                     case 1:
-                        $scope.categories.day = 'Yesterday';
+                        $scope.dayName = 'Yesterday';
                         break;
+                        // Otherwise retrieve day's name
+                    default:
+                        $scope.dayName = $scope.days[$scope.activeDay];
                 }
             };
 
 
             $scope.prev = function() {
-                if (activeDay < 1) {
+                // Don't let browse behind Monday
+                if ($scope.activeDay < 1) {
                     return false;
                 } else {
-                    activeDay = activeDay - 1;
-                    $scope.categories = $scope.data[activeDay];
+                    // Set view to day before
+                    $scope.activeDay = $scope.activeDay - 1;
+
                     $scope.nameDays();
                 }
-                return activeDay;
+                return $scope.activeDay;
             };
 
             $scope.next = function() {
-
-                if (activeDay > 3) {
+                // Don't let browse ahead of Friday
+                if ($scope.activeDay > 3) {
                     return false;
                 } else {
-                    activeDay = activeDay + 1;
-                    $scope.categories = $scope.data[activeDay];
+                    // Set view to day after
+                    $scope.activeDay = $scope.activeDay + 1;
                     $scope.nameDays();
                 }
-                return activeDay;
+                return $scope.activeDay;
             };
-
-            /* Get data from the factory Categories
-             * getList() is a restangular method
-             * Passing response to scope once promise is successful
-             */
-            // (function init() {
-            //     Categories.getList().then(function(categories) {
-            //         $scope.categories = computeAverageDishRatings(categories);
-            //     });
-            // })();
 
 
         }
