4f13fd1feadb2e0586b9d102982690323d498560
Luca Trinca <lucatrinca.uk@gmail.com>
2014-12-05 23:08:18 +0000

    Fix save back to Factory

--- app/scripts/admin/adminController.js
+++ app/scripts/admin/adminController.js
@@ -1,19 +1,18 @@
 angular.module('Boom')
-    .controller('adminController', ['$scope', 'categories', 'dishes', '$filter', '$state', '$stateParams',
-        function($scope, categories, dishes, $filter, $state, $stateParams) {
+    .controller('adminController', ['$scope', 'categories', 'dishes', '$filter', '$state', 'Dishes',
+        function($scope, categories, dishes, $filter, $state, Dishes) {
             'use strict';
 
             // Load categories
             $scope.categories = categories;
             $scope.dishes = dishes;
 
-
             $scope.addDish = {};
 
             $scope.save = function() {
                 $scope.addDish.id = $filter('dashify')($scope.addDish.title);
 
-                //Dishes.saveDish($scope.addDish);
+                Dishes.saveDish($scope.addDish);
                 $state.go('app.admin.dishes');
             };
 
