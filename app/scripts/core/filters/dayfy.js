bc236ec6b9c71746979040fd08964c35388d9bfb
Luca Trinca <lucatrinca.uk@gmail.com>
2014-12-10 20:27:45 +0000

    Filter to return day without fancy name

--- app/scripts/core/filters/dayfy.js
+++ app/scripts/core/filters/dayfy.js
@@ -9,25 +9,33 @@ angular.module('Boom').filter('dayfy', ['$rootScope', function($rootScope) {
 		dayName;
 
 
-	return function(activeDay) {
 
-		switch ($rootScope.day - activeDay) {
-			// If view is same as current day, set it as Today
-			case 0:
-				dayName = 'Today';
-				break;
-				// If view is 1 day behind current day, set it as Yesterday
-			case -1:
-				dayName = 'Tomorrow';
-				break;
-				// If view is 1 day ahead current day, set it as Tomorrow
-			case 1:
-				dayName = 'Yesterday';
-				break;
-				// Otherwise retrieve day's name
-			default:
-				dayName = days[activeDay];
+	return function(activeDay, originalDay) {
+		// return name of the day not related to the active view
+		// e.g. not 'Today', but 'Monday'
+		if (originalDay) {
+			dayName = days[activeDay];
+		} else {
+
+			switch ($rootScope.day - activeDay) {
+				// If view is same as current day, set it as Today
+				case 0:
+					dayName = 'Today';
+					break;
+					// If view is 1 day behind current day, set it as Yesterday
+				case -1:
+					dayName = 'Tomorrow';
+					break;
+					// If view is 1 day ahead current day, set it as Tomorrow
+				case 1:
+					dayName = 'Yesterday';
+					break;
+					// Otherwise retrieve day's name
+				default:
+					dayName = days[activeDay];
+			}
 		}
+
 		return dayName;
 	};
 }]);
