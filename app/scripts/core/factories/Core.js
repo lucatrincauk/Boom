00774d12463993116f009e9052b44e358ce5ac4d
Luca Trinca <lucatrinca.uk@gmail.com>
2014-12-15 12:40:30 +0000

    Commenting Core.js

--- app/scripts/core/factories/Core.js
+++ app/scripts/core/factories/Core.js
@@ -5,8 +5,9 @@ angular.module('Boom')
 	function() {
 
 
-		// get today's date and remove sunday
+		// get today's date and make it start on Monday
 		var day = new Date().getDay() - 1,
+			// bootstrapping canteen to WS
 			canteenName = 'Waterside',
 			cycle;
 
@@ -14,14 +15,39 @@ angular.module('Boom')
 		if (day === -1) {
 			day = 6;
 		}
-
+		/*
+		 * Gets current week number
+		 * Week starts on Saturday
+		 * This will show the next week menu already on Saturday
+		 * e.g.Fri Dec 12 / 2014 is Day 346 and Week 50(cycle 2)
+		 * e.g. Sat Dec 13/2014 is Day 347 and Week 51 (cycle 3)
+		 * The script will go up to 53 weeks
+		 * which is fine as 53 % 4 is the same as 1 % 4 (equals to cycle 1)
+		 */
 		var getWeek = function() {
 			Date.prototype.getWeek = function() {
+				/* 
+				 * Get the full 1st day of the year
+				 * e.g. Wed Jan 01 2014 00:00:00 GMT+0000 (GMT)
+				 */
 				var onejan = new Date(this.getFullYear(), 0, 1);
+				/*
+				 * returns (today's date - first day of the year) / 86400000 which is the milliseconds in one day) = current day number
+				 * then, (current day number + include days of the first week) / 7 = total number of weeks
+				 */
 				return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 			};
 			return new Date().getWeek();
 		};
+		/*
+		 * Gets current cycle
+		 * Menus are split in cycles of 4, repeating across the year
+		 * Getting the week number and using the modulus operator
+		 * E.g. 51 % 4 = 3
+		 * Cycle can be compared against this table: https://confluence.devops.mnscorp.net/display/SE/JSON+Menu
+		 * MSQ is 2 weeks behind, so when WS is on Cycle 4, MSQ is on Cycle 2
+		 * Cycle is required to show the current week's menu
+		 */
 		var getCycle = function() {
 			if (canteenName === 'Waterside') {
 				switch (getWeek() % 4) {
@@ -59,7 +85,10 @@ angular.module('Boom')
 		};
 
 		var isClosed = function() {
-
+			/*
+			 * if day is ahead of Friday (4)
+			 * set it as true
+			 */
 			if (day > 4) {
 				return true;
 			} else {
