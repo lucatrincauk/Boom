5ce25611bea417ef9601435046c6d6c64e82aaec
Luca Trinca <lucatrinca.uk@gmail.com>
2015-01-05 12:40:51 +0000

    show user role only if admin

--- app/scripts/users/userProfileController.js
+++ app/scripts/users/userProfileController.js
@@ -2,13 +2,11 @@ angular.module('Boom')
 	.controller('userProfileController', ['$scope', 'users', '$firebase', '$firebaseAuth', 'FirebaseUrl',
 		function($scope, users, $firebase, $firebaseAuth, FirebaseUrl) {
 			'use strict';
-
 			var ref = new Firebase(FirebaseUrl);
 
 			ref.onAuth(function() {
 				$scope.user = users.getUser();
 			});
-			//	$scope.user = users.getUser();
 
 			$scope.save = function() {
 				$scope.user.$save().then(function() {
