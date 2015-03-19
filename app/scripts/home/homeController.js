753e74cce97c2590f1bf1d3908e20056a37a3de7
lucatrincauk <lucatrinca.uk@gmail.com>
2015-03-19 16:29:53 +0000

    Day Swipe UX

--- app/scripts/home/homeController.js
+++ app/scripts/home/homeController.js
@@ -1,13 +1,40 @@
 angular.module('Boom')
-    .controller('homeController', ['$scope', '$rootScope', 'core', 'categories', 'dishes', '$filter', 'messageCenterService', '$timeout', 'Dishes',
-        function($scope, $rootScope, core, categories, dishes, $filter, messageCenterService, $timeout, Dishes) {
+    .controller('homeController', ['$scope', '$rootScope', 'core', 'categories', 'dishes', '$filter', 'messageCenterService', '$timeout', 'Dishes','$ionicSlideBoxDelegate',
+        function($scope, $rootScope, core, categories, dishes, $filter, messageCenterService, $timeout, Dishes, $ionicSlideBoxDelegate) {
             'use strict';
+            // Show initial Loading screen
+            $rootScope.$broadcast('loading:show');
 
-            // Assign data to scope
-            $scope.dishes = dishes;
-            $scope.categories = categories;
+            // Once dishes come back from server..
+            dishes.$loaded(function() {
+
+                // Assign data to scope         
+                $scope.dishes = dishes;
+
+                // if it's weekend (sat: day = 5, sun: day = -1)
+                if (core.isClosed()) {
+                    // Show message "Canteen closed"
+                    $timeout(function() {
+                        messageCenterService.add('danger', $scope.canteenName + ' is closed today. Showing next week');
+                    });
+                    // Show Monday
+                   $ionicSlideBoxDelegate.slide(0);
+                } else {
+                    // Otherwise, show Today
+                    $ionicSlideBoxDelegate.slide(core.day);
+                }
+                // Hide Loading Screen
+                $rootScope.$broadcast('loading:hide');
+                // Set fancy day names
+                $scope.nameDays();
+            });
+
+            categories.$loaded(function() {
+                $scope.categories = categories;
+            });
             $scope.canteenName = core.canteenName();
             $scope.cycle = core.cycle(true);
+            $scope.days = core.days;
             // Updating view when canteen changes
             $rootScope.$on('canteenChanged', function() {
                 $scope.cycle = core.cycle(true);
@@ -18,64 +45,42 @@ angular.module('Boom')
 
             });
             $scope.nameDays = function() {
-                $scope.dayName = $filter('dayfy')($scope.activeDay);
+                $scope.dayName = $filter('dayfy')($ionicSlideBoxDelegate.currentIndex());
             };
 
 
             $scope.prev = function() {
                 // Don't let browse behind Monday
-                if ($scope.activeDay < 1) {
-                    return false;
-                } else {
-                    // Set view to day before
-                    $scope.activeDay = $scope.activeDay - 1;
+                // if ($scope.activeDay < 1) {
+                //     return false;
+                // } else {
+                //     // Set view to day before
+                //     $scope.activeDay = $scope.activeDay - 1;
 
-                    $scope.nameDays();
-                }
-                return $scope.activeDay;
+                //     $scope.nameDays();
+                // }
+                // return $scope.activeDay;
+                $ionicSlideBoxDelegate.previous();
+                 $scope.nameDays();
             };
 
             $scope.next = function() {
                 // Don't let browse ahead of Friday
-                if ($scope.activeDay > 3) {
-                    return false;
-                } else {
-                    // Set view to day after
-                    $scope.activeDay = $scope.activeDay + 1;
-                    $scope.nameDays();
-                }
-                return $scope.activeDay;
-            };
-
-            $scope.dayFilter = function(dish) {
-                // Removing initial loading veil
-                if ($rootScope.veil === 1) {                    
-                    $rootScope.$broadcast('loading:hide');
-                    $rootScope.veil = 0;
-                }
-                return dish.week[$scope.cycle][core.days[$scope.activeDay]];
-            };
-
-            $scope.init = (function() {
-                // Adding initial loading veil
-                $rootScope.veil = 1;
-                $rootScope.$broadcast('loading:show');
-                // if it's weekend (sat: day = 5, sun: day = -1)
-
-                if (core.isClosed()) {
-                    $timeout(function() {
-                        messageCenterService.add('danger', $scope.canteenName + ' is closed today. Showing next week');
-                    });
-                    // set view to Monday
-                    $scope.activeDay = 0;
-                } else {
-                    // otherwise set view to current day
-                    $scope.activeDay = core.day;
-                }
-
+                // if ($scope.activeDay > 3) {
+                //     return false;
+                // } else {
+                //     // Set view to day after
+                //     $scope.activeDay = $scope.activeDay + 1;
+                //     
+                // }
+                // return $scope.activeDay;
+                $ionicSlideBoxDelegate.next();
                 $scope.nameDays();
 
-            })();
+            };
+
+    
+           
 
 
         }
